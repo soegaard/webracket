e4dec6bf5f35a56f95b9c5ce8f00cb0aebf01f67
Jens Axel Søgaard <jensaxel@soegaard.net>
2025-08-09 12:14:23 +0200

    Prepare for compilation of files from command line

--- webracket.rkt
+++ webracket.rkt
@@ -1,35 +1,56 @@
 #lang racket
+;;;
+;;; COMMAND LINE TOOL
+;;;
 
-;;;
-;;; The command line tool `webracket`
-;;;
+;; This file handles the parsing of the command line.
+;; Based on the flags we set various parameters and
+;; pass these on to the driver in "driver.rkt" which
+;; handles file operations (and invokes the compiler).
+
+;; usage: racket webracket.rkt [ <option> ... ] [<files>] ...
+
+(require "driver.rkt")
+
 
 (define verbose-mode   (make-parameter #f))
-(define profiling-on   (make-parameter #f))
-(define optimize-level (make-parameter 0))
-(define link-flags     (make-parameter null))
- 
+(define profiling-on   (make-parameter #f))   ; ignored
+(define link-flags     (make-parameter '()))  ; ignored
+(define run-after      (make-parameter #f))
+
+(define browser        (make-parameter #f))
+(define nodejs         (make-parameter #t))   ; default
+
 (define file-to-compile
   (command-line
    #:program "webracket"
-   #:once-each
+
+   #:once-each ; independent flags
+   [("-r" "--run")     "Run the program after compilation."
+                       (run-after #t)]
    [("-v" "--verbose") "Compile with verbose messages"
                        (verbose-mode #t)]
    [("-p" "--profile") "Compile with profiling"
                        (profiling-on #t)]
-   #:once-any
-   [("-o" "--optimize-1") "Compile with optimization level 1"
-                          (optimize-level 1)]
-   ["--optimize-2"        (; show help on separate lines
-                           "Compile with optimization level 2,"
-                           "which includes all of level 1")
-                          (optimize-level 2)]
-   #:multi
+
+   #:once-any ; only one flag from this group
+   [("-b" "--browser") "Generate code for browser."
+                       (browser #t)]
+   [("-n" "--node")    "Generate code for Node.js"
+                       (nodejs #t)]
+                          
+   #:multi ; can be used multiple times
    [("-l" "--link-flags") lf ; flag takes one argument
                           "Add a flag <lf> for the linker"
                           (link-flags (cons lf (link-flags)))]
+   
    #:args (filename) ; expect one command-line argument: <filename>
    ; return the argument as a filename to compile
    filename))
 
-(displayln file-to-compile)
+
+(drive-compilation #:filename   filename
+                   #:verbose?   (verbose-mode)
+                   #:browser?   (browser)
+                   #:node?      (nodejs)
+                   #:run-after? (run-after))
