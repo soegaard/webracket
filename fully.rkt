4d081505775dfcf0aa2f67e70414cbb15c3f1a99
Jens Axel Søgaard <jensaxel@soegaard.net>
2025-08-20 17:45:45 +0200

    Add fully.rkt

---
+++ fully.rkt
@@ -0,0 +1,33 @@
+#lang racket/base
+
+(require '#%kernel)
+
+(provide #%declare
+         #%expression
+         #%plain-module-begin
+         #%provide
+         #%require
+         #%plain-app
+         #%top
+         #%variable-reference
+         
+         begin
+         define-values
+         #%plain-lambda
+         case-lambda
+         if
+         begin
+         begin0
+         let-values
+         letrec-values
+         set!
+         quote
+         with-continuation-mark
+         
+
+         begin-for-syntax
+         define-syntaxes
+         quote-syntax
+         
+         module
+         module*)
