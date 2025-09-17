e17e357c164cc1b7893f1112b56cd2d69906dd84
Jens Axel Søgaard <jensaxel@soegaard.net>
2025-09-18 01:38:44 +0200

    open-input-string

--- compiler.rkt
+++ compiler.rkt
@@ -3413,6 +3413,8 @@
          [(string-copy!)               (inline-prim/optional sym ae1 3 5)]
          [(bytes-copy!)                (inline-prim/optional sym ae1 3 5)]
 
+         [(open-input-string)          (inline-prim/optional sym ae1 1 2)]
+
          [(make-vector)                (inline-prim/optional sym ae1 1 2)]
          [(make-string)                (inline-prim/optional sym ae1 1 2)]
          [(make-bytes)                 (inline-prim/optional sym ae1 1 2)]
