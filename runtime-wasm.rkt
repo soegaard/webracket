c7237215c57f74885d98b702ab4fd537b3b1138a
Jens Axel Søgaard <jensaxel@soegaard.net>
2025-09-26 21:38:20 +0200

    Improve bytes

--- runtime-wasm.rkt
+++ runtime-wasm.rkt
@@ -8199,10 +8199,10 @@
                ;; Populate array from arguments or list elements
                (local.set $i (i32.const 0))
                (block $done
-                      (loop $loop
-                            (br_if $done (i32.ge_u (local.get $i) (local.get $len)))
-                            (if (i32.eqz (local.get $use-args?))
-                                (then
+                      (if (i32.eqz (local.get $use-args?))
+                          (then
+                           (loop $loop-list
+                                 (br_if $done (i32.ge_u (local.get $i) (local.get $len)))
                                  (if (ref.test (ref $Pair) (local.get $node))
                                      (then
                                       (local.set $pair (ref.cast (ref $Pair) (local.get $node)))
@@ -8210,29 +8210,47 @@
                                       (local.set $node (struct.get $Pair $d (local.get $pair))))
                                      (else
                                       (call $raise-pair-expected (local.get $node))
-                                      (unreachable))))
-                                (else
-                                 (local.set $x (array.get $Args (local.get $as) (local.get $i)))))
-                            (if (ref.test (ref i31) (local.get $x))
-                                (then
-                                 (local.set $v (i31.get_u (ref.cast (ref i31) (local.get $x))))
-                                 (if (i32.eqz (i32.and (local.get $v) (i32.const 1)))
+                                      (unreachable)))
+                                 (if (ref.test (ref i31) (local.get $x))
                                      (then
-                                      (local.set $v (i32.shr_u (local.get $v) (i32.const 1)))
-                                      (if (i32.lt_u (local.get $v) (i32.const 256))
+                                      (local.set $v (i31.get_u (ref.cast (ref i31) (local.get $x))))
+                                      (if (i32.eqz (i32.and (local.get $v) (i32.const 1)))
                                           (then
-                                           (call $i8array-set! (local.get $arr) (local.get $i) (local.get $v)))
+                                           (local.set $v (i32.shr_u (local.get $v) (i32.const 1)))
+                                           (if (i32.lt_u (local.get $v) (i32.const 256))
+                                               (then
+                                                (call $i8array-set! (local.get $arr) (local.get $i) (local.get $v)))
+                                               (else
+                                                (call $raise-byte-out-of-range (local.get $x))
+                                                (unreachable))))
                                           (else
-                                           (call $raise-byte-out-of-range (local.get $x))
+                                           (call $raise-check-fixnum (local.get $x))(unreachable)))
+                                 (local.set $i (i32.add (local.get $i) (i32.const 1)))
+                                 (br $loop-list)))))
+                          (else
+                           (loop $loop-args
+                                 (br_if $done (i32.ge_u (local.get $i) (local.get $len)))
+                                 (local.set $x (array.get $Args (local.get $as) (local.get $i)))
+                                 (if (ref.test (ref i31) (local.get $x))
+                                     (then
+                                      (local.set $v (i31.get_u (ref.cast (ref i31) (local.get $x))))
+                                      (if (i32.eqz (i32.and (local.get $v) (i32.const 1)))
+                                          (then
+                                           (local.set $v (i32.shr_u (local.get $v) (i32.const 1)))
+                                           (if (i32.lt_u (local.get $v) (i32.const 256))
+                                               (then
+                                                (call $i8array-set! (local.get $arr) (local.get $i) (local.get $v)))
+                                               (else
+                                                (call $raise-byte-out-of-range (local.get $x))
+                                                (unreachable))))
+                                          (else
+                                           (call $raise-check-fixnum (local.get $x))
                                            (unreachable))))
                                      (else
                                       (call $raise-check-fixnum (local.get $x))
-                                      (unreachable))))
-                                (else
-                                 (call $raise-check-fixnum (local.get $x))
-                                 (unreachable)))
-                            (local.set $i (i32.add (local.get $i) (i32.const 1)))
-                            (br $loop)))
+                                      (unreachable)))
+                                 (local.set $i (i32.add (local.get $i) (i32.const 1)))
+                                 (br $loop-args)))))
                ;; Wrap in Bytes struct and return
                (struct.new $Bytes
                            (i32.const 0)
