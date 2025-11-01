a0f828de3cdb5ac5ad3a91ec347d326185f7abe9
Jens Axel Søgaard <jensaxel@soegaard.net>
2025-11-01 11:18:12 +0100

    Custom writer for variables.

--- structs.rkt
+++ structs.rkt
@@ -8,7 +8,14 @@
 ; This module contains structure definitions used by multiple modules.
 
 ; See "compiler.rkt"
-(struct variable (id) #:transparent)
+(struct variable (id)
+  #:transparent
+  #:methods gen:custom-write
+  [(define (write-proc d port mode)
+     ;; delegate to Racket's printer for the value field
+     ; (write `(variable: ,(syntax-e (variable-id d))) port)
+     (write (syntax-e (variable-id d)) port)
+     )])
 
 
 (struct foreign (racket-name    ; the Racket primitive, we are defining (symbol)
