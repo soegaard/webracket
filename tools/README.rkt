b5730ddbf102d347185f605ded51f73461efbf42
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-01-16 02:54:46 +0100

    Add readme

---
+++ tools/README.rkt
@@ -0,0 +1,12 @@
+Tools
+=====
+
+This folder holds various useful tools.
+
+For now, there is one tool here.
+
+
+pretty.rkt
+----------
+Reads s-expressions from an input port and pretty-prints to an output port.
+
