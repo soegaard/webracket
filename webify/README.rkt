1babd19642968a8710dd9e44e4cf0ca8dcca3429
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-01-16 02:57:46 +0100

    Add readme

---
+++ webify/README.rkt
@@ -0,0 +1,7 @@
+Webify
+======
+
+This folder contains a port of `schemify` from the Racket to Chez Scheme compiler.
+Eventually, this `webify` will be integrated with the compiler in order to support modules.
+
+This is a work in progress.
