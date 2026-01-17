ef29e8f6f22f3de5b020fee3c0f17df3fbc5308e
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-01-17 03:05:48 +0100

    More tests

--- test/test-fixnums.rkt
+++ test/test-fixnums.rkt
@@ -4,12 +4,25 @@
        (equal? (fx* -2 3 4) -24)
        (equal? (fx* 1) 1)
        (equal? (fx* 1 2 3 4) 24))
+ 
+ (list "arguments are checked"
+       (with-handlers ([(λ (_) #t) (lambda (e)
+                                     (string=? (exn-message e)
+                                               "fx*expected fixnum, got: 1.073741e9"))])
+         (fx* (expt 2 30) 1))
+       
+       (with-handlers ([(λ (_) #t) (lambda (e)
+                                     (string=? (exn-message e)
+                                               "fx*expected fixnum, got: 1.073741e9"))])
+         (fx* 1 (expt 2 30))))
+ 
  (list "fx* overflow"
        (with-handlers ([exn:fail? (lambda (e)
                                     (string=? (exn-message e)
-                                              "fx*: fixnum overflow with arguments 1073741824 and 1073741824"))])
-         (fx* (expt 2 30) (expt 2 30))
+                                              "fx*: fixnum overflow with arguments -536870912 and -536870912"))])
+         (fx* (expt 2 29) (expt 2 29))
          #f))
+ 
  (list "fx* boundaries"
        (let* ([a (sub1 (expt 2 14))]
               [b (sub1 (expt 2 15))]
