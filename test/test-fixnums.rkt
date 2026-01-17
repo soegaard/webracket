58642ac5b48439af15cd74654498f1f6df19fdb5
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-01-17 16:13:02 +0100

    Repair tests

--- test/test-fixnums.rkt
+++ test/test-fixnums.rkt
@@ -1,3 +1,7 @@
+
+(define MIN -536870912) ; (- (expt 2 29))
+(define MAX  536870911) ; (- (expt 2 29) 1)
+
 (list
  (list "fx*"
        (equal? (fx* 2 3) 6)
@@ -20,7 +24,7 @@
        (with-handlers ([exn:fail? (lambda (e)
                                     (string=? (exn-message e)
                                               "fx*: fixnum overflow with arguments -536870912 and -536870912"))])
-         (fx* (expt 2 29) (expt 2 29))
+         (fx* MAX MAX)
          #f))
  
  (list "fx* boundaries"
@@ -58,7 +62,49 @@
                                                "fx-expected fixnum, got: 1.073741e9"))])
          (fx- 1 (expt 2 30))))
 
- (list "fx/ arguments are checked"
+
+ (list "fx+ overflow"
+       (with-handlers ([exn:fail? (lambda (e)
+                                    (string=? (exn-message e)
+                                              "fx+: fixnum overflow with arguments 536870911 and 1"))])
+         (fx+ MAX 1)
+         #f))
+
+ (list "fx- overflow"
+       (with-handlers ([exn:fail? (lambda (e)
+                                    (string=? (exn-message e)
+                                              "fx-: fixnum overflow with arguments -536870912 and 1"))])
+         (fx- MIN 1)
+         #f))
+
+
+ (list "fx+ boundaries"
+       (let* ([a   MAX]
+              [sum (fx+ a 0)])
+         (and (fixnum? sum)
+              (equal? sum a)))
+
+       (let* ([a MIN]
+              [sum (fx+ a 0)])
+         (and (fixnum? sum)
+              (equal? sum a))))
+
+ (list "fx- boundaries"
+       (let* ([a    MAX]
+              [diff (fx- a 0)])
+         (and (fixnum? diff)
+              (equal? diff a)))
+       
+       (let* ([a    MIN]
+              [diff (fx- a 0)])
+         (and (fixnum? diff)
+              (equal? diff a))))
+)
+
+
+;; There is no fx/ in full racket
+
+ #;(list "fx/ arguments are checked"
        (with-handlers ([(λ (_) #t) (lambda (e)
                                      (string=? (exn-message e)
                                                "fx/expected fixnum, got: 1.073741e9"))])
@@ -68,53 +114,19 @@
                                                "fx/expected fixnum, got: 1.073741e9"))])
          (fx/ 1 (expt 2 30))))
 
- (list "fx+ overflow"
-       (with-handlers ([exn:fail? (lambda (e)
-                                    (string=? (exn-message e)
-                                              "fx+: fixnum overflow with arguments 536870911 and 1"))])
-         (fx+ (sub1 (expt 2 29)) 1)
-         #f))
-
- (list "fx- overflow"
-       (with-handlers ([exn:fail? (lambda (e)
-                                    (string=? (exn-message e)
-                                              "fx-: fixnum overflow with arguments -536870912 and 1"))])
-         (fx- (- (expt 2 29)) 1)
-         #f))
-
- (list "fx/ overflow"
+ #;(list "fx/ overflow"
        (with-handlers ([exn:fail? (lambda (e)
                                     (string=? (exn-message e)
                                               "fx/: fixnum overflow with arguments -536870912 and -1"))])
          (fx/ (- (expt 2 29)) -1)
          #f))
 
- (list "fx/ division by zero"
+ #;(list "fx/ division by zero"
        (with-handlers ([exn:fail? (lambda (_) #t)])
          (fx/ 1 0)
          #f))
 
- (list "fx+ boundaries"
-       (let* ([a (sub1 (expt 2 29))]
-              [sum (fx+ a 0)])
-         (and (fixnum? sum)
-              (equal? sum a)))
-       (let* ([a (- (expt 2 29))]
-              [sum (fx+ a 0)])
-         (and (fixnum? sum)
-              (equal? sum a))))
-
- (list "fx- boundaries"
-       (let* ([a (sub1 (expt 2 29))]
-              [diff (fx- a 0)])
-         (and (fixnum? diff)
-              (equal? diff a)))
-       (let* ([a (- (expt 2 29))]
-              [diff (fx- a 0)])
-         (and (fixnum? diff)
-              (equal? diff a))))
-
- (list "fx/ boundaries"
+ #;(list "fx/ boundaries"
        (let* ([a (sub1 (expt 2 29))]
               [q (fx/ a 1)])
          (and (fixnum? q)
@@ -122,4 +134,4 @@
        (let* ([a (- (expt 2 29))]
               [q (fx/ a 1)])
          (and (fixnum? q)
-              (equal? q a)))))
+              (equal? q a))))
