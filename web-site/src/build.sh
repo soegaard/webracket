5f69ff1559b1bbcb83636b015b85a46b8a273e7c
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-01-24 01:13:17 +0100

    Add build script

---
+++ web-site/src/build.sh
@@ -0,0 +1,8 @@
+echo "-- Compiling web-site.rkt --"
+racket ../../webracket.rkt --browser --ffi dom --stdlib web-site.rkt
+
+echo "-- Copying Assets --"
+cp web-site.wasm ../public
+cp web-site.html ../public
+
+echo "-- Done --"
