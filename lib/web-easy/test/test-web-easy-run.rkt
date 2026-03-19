3a4cb50867fa02c89ddd3a24747727e9211079ba
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-03-19 02:05:35 +0100

    Tests

--- lib/web-easy/test/test-web-easy-run.rkt
+++ lib/web-easy/test/test-web-easy-run.rkt
@@ -6,3 +6,4 @@
 
 (include/reader "../main.rkt" read-syntax/skip-first-line)
 (include/reader "test-web-easy-body.rkt" read-syntax/skip-first-line)
+(include/reader "test-html-elements-body.rkt" read-syntax/skip-first-line)
--- lib/web-easy/test/test-web-easy.rkt
+++ lib/web-easy/test/test-web-easy.rkt
@@ -8,3 +8,4 @@
 
 (include/reader "../main.rkt" read-syntax/skip-first-line)
 (include/reader "test-web-easy-body.rkt" read-syntax/skip-first-line)
+(include/reader "test-html-elements-body.rkt" read-syntax/skip-first-line)
