873f1017950332ff6bde243bbe950ebb29c9cd31
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-03-19 02:04:47 +0100

    Include define-element.rkt

--- lib/web-easy/main-browser.rkt
+++ lib/web-easy/main-browser.rkt
@@ -6,6 +6,7 @@
 
 (include-lib define)
 
+(include/reader "define-element.rkt" read-syntax/skip-first-line)
 (include/reader "observable.rkt"      read-syntax/skip-first-line)
 (include/reader "operator.rkt"        read-syntax/skip-first-line)
 (include/reader "view.rkt"            read-syntax/skip-first-line)
