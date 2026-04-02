9666ba6596b18423bc9854d7ea53a35be6b72260
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-04-02 18:50:20 +0200

    Remove duplicate WebSocket label export

--- scribblings/lib-websocket-labels.rkt
+++ scribblings/lib-websocket-labels.rkt
@@ -1,5 +1,42 @@
 #lang racket/base
 
-(require "websocket-labels.rkt")
+(require racket/contract/base)
 
-(provide (all-from-out "websocket-labels.rkt"))
+(provide
+  (struct-out websocket)
+  websocket-new
+  check-websocket
+  websocket-url
+  websocket-ready-state
+  websocket-ready-state-number
+  websocket-buffered-amount
+  websocket-protocol
+  websocket-extensions
+  websocket-send
+  websocket-close
+  websocket-onopen!
+  websocket-onmessage!
+  websocket-onclose!
+  websocket-onerror!
+  websocket-add-event-listener!
+  websocket-remove-event-listener!
+  (for-label (all-defined-out)))
+
+(struct websocket (raw) #:transparent)
+
+(define websocket-new any/c)
+(define check-websocket any/c)
+(define websocket-url any/c)
+(define websocket-ready-state any/c)
+(define websocket-ready-state-number any/c)
+(define websocket-buffered-amount any/c)
+(define websocket-protocol any/c)
+(define websocket-extensions any/c)
+(define websocket-send any/c)
+(define websocket-close any/c)
+(define websocket-onopen! any/c)
+(define websocket-onmessage! any/c)
+(define websocket-onclose! any/c)
+(define websocket-onerror! any/c)
+(define websocket-add-event-listener! any/c)
+(define websocket-remove-event-listener! any/c)
