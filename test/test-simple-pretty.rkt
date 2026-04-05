e43647f4b6d711292418b8bbd98efcfca53f30a0
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-04-05 14:21:37 +0200

    Minimal smoke test for (include-lib simple-pretty)

--- test/test-simple-pretty.rkt
+++ test/test-simple-pretty.rkt
@@ -1,3 +1,20 @@
+;;;
+;;; simple-pretty smoke test
+;;;
 
-(js-log (string? "foo"))
+;; Minimal smoke test for `(include-lib simple-pretty)`.
+;;
+;; Build:
+;;   racket -l errortrace -t ../webracket.rkt -- --ffi ../ffi/standard.ffi -r simple-test.rkt
 
+(include-lib simple-pretty)
+
+(define (check-equal got want label)
+  (unless (equal? got want)
+    (error 'check-equal (format "~a: got ~s want ~s" label got want))))
+
+(list
+ (list "simple-pretty include-lib"
+       (let ()
+         (check-equal (simple-pretty-format '(1 2 3)) "(1 2 3)" "format smoke test")
+         #t)))
