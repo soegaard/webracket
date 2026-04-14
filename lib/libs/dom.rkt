6790d2b8be3bcdce1a80c543c27e7e4d9184efce
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-04-14 10:43:20 +0200

    Remove performance and canvas from dom

--- lib/libs/dom.rkt
+++ lib/libs/dom.rkt
@@ -8,10 +8,8 @@
 ;; callers one Rackety entry point for the DOM surface.
 
 (include-lib window)
-(include-lib performance)
 (include-lib document)
 (include-lib domrect)
 (include-lib element)
-(include-lib canvas)
 (include-lib image)
 (include-lib event)
