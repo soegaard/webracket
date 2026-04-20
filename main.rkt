16336cb92c176dd87ea4c1e683670a32c4339588
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-04-21 00:48:53 +0200

    Provide racket/splicing

--- main.rkt
+++ main.rkt
@@ -1,9 +1,10 @@
 #lang racket/base
 
 (require "core.rkt"
-         "browser-options.rkt"
+         (for-syntax racket/splicing)
          (for-syntax "browser-options.rkt"))
 (provide (all-from-out "core.rkt")
+         (for-syntax (all-from-out racket/splicing))
          (for-syntax (all-from-out "browser-options.rkt")))
 
 ;; Uncommenting the line below disables the custom reader
